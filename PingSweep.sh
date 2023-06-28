1ce9235d1ef0789bf1124858ea0b51244c4c1136
Tom R <137434709+mr-tomr@users.noreply.github.com>
2023-06-28 10:40:04 -0400

    Update PingSweep.sh
    Updated comments.

--- PingSweep.sh
+++ PingSweep.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-# Uses a file named hosts.txt for the list of nodes to check
-#Performs a basic, but quick, ping sweep.
+# Uses a file named hosts.txt for the list of nodes to check.
+# Performs a basic, but quick, ping sweep.
+# Use additional port scanning to verify if a host is online.
 
 awk '{print $1}' < hosts.txt | while read ip; do
     if ping -c1 $ip >/dev/null 2>&1; then
