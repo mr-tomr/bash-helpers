e876f35c9b446ae4408bf585f54cc2194e0bbdbb
Tom R <137434709+mr-tomr@users.noreply.github.com>
2024-01-01 17:44:41 -0500

    Added Oneliner

--- PingSweep.sh
+++ PingSweep.sh
@@ -3,6 +3,9 @@
 # Performs a basic, but quick, ping sweep.
 # Use additional port scanning to verify if a host is online.
 
+# One Liner with Onscreen Progress
+# subnet="172.16.200"; for host in {1..254}; do ping -c 1 -w 1 "$subnet.$host" >/dev/null 2>&1 && echo -n "#" || echo -n "."; done; echo
+
 awk '{print $1}' < hosts.txt | while read ip; do
     if ping -c1 $ip >/dev/null 2>&1; then
         echo $ip IS UP
