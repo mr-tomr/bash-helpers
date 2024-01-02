df3c7c2716ba7feb393a7d603e1fb8d30daccadd
Tom R <137434709+mr-tomr@users.noreply.github.com>
2024-01-01 22:54:53 -0500

    Updated Status Bar

--- PingSweep.sh
+++ PingSweep.sh
@@ -4,7 +4,7 @@
 # Use additional port scanning to verify if a host is online.
 
 # One Liner with Onscreen Progress
-# subnet="172.16.200"; for host in {1..254}; do if ping -c 1 -w 1 "$subnet.$host" >/dev/null 2>&1; then echo "$subnet.$host"; fi; done
+# subnet="172.16.211"; for host in {1..254}; do if ping -c 1 -w 1 "$subnet.$host" >/dev/null 2>&1; then echo -n "$subnet.$host "; else echo -n "."; fi; done; echo
 
 
 awk '{print $1}' < hosts.txt | while read ip; do
