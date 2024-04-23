b0eec576419a1ec1547ed5f7f5c084d076883823
Tom R <137434709+mr-tomr@users.noreply.github.com>
2024-04-23 14:02:33 -0400

    Updated FindWritableDirectoriesLikeWeb.sh

--- FindWritableDirectoriesLikeWeb.sh
+++ FindWritableDirectoriesLikeWeb.sh
@@ -1,6 +1,7 @@
 # Created by Tom R.
 
 # Find directories you can load webshells in to, etc.
+# In this case, I could start the web service using sudo.  www user was in root group.
 
 
 find /usr/local/www/apache24 -type d -perm -o=w
