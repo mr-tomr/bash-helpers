6a844158d3e1dc456e268487228efcff3791ede5
Tom R <137434709+mr-tomr@users.noreply.github.com>
2024-05-16 21:14:51 -0400

    Create SQL-SourceCodeSearch.sh

---
+++ SQL-SourceCodeSearch.sh
@@ -0,0 +1,8 @@
+# Created by Tom R.
+# Basic Search Option for finding SQL commands in source code.
+# Documented 20240516
+
+# If you download source code, use this technique to find things like SQL commands, etc.
+
+grep -r 'SELECT' .
+
