5ea9f4f50cd293b0609f6ab56dc8c15472b971dc
Tom R <137434709+mr-tomr@users.noreply.github.com>
2024-05-31 13:52:36 -0400

    Create SystemInfoFull.sh

---
+++ SystemInfoFull.sh
@@ -0,0 +1,5 @@
+# Created by Tom R.
+
+# Top system information commands
+
+(echo "uname -a:" && uname -a && echo && echo "/etc/os-release:" && cat /etc/os-release && echo && echo "lsb_release -a:" && lsb_release -a 2>/dev/null && echo && echo "hostnamectl:" && hostnamectl)
